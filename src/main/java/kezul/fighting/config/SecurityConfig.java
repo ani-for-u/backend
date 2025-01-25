c8af23f96aafe3ed6063d083f6ee30404b2208f8
sanghoon <tkdgns5817@gmail.com>
2025-01-26 03:27:49 +0900

    [chore][#2] 스프링 시큐리티 기본 설정
    1. csrf, formLogin, httpBasic 무효화
    2. session을 stateless하게 설정(JWT 사용 예정)

--- src/main/java/kezul/fighting/config/SecurityConfig.java
+++ src/main/java/kezul/fighting/config/SecurityConfig.java
@@ -2,13 +2,35 @@ package kezul.fighting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity // 이게 뭔지 검색 필요
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
 }
