c9ca4a9125adf21995e98506f9cee6506e6fb6b2
sanghoon <tkdgns5817@gmail.com>
2025-01-26 10:29:06 +0900

    [fix][#4] 공통 컴포넌트 개발
    1. 공통 응답 dto 생성
    2. ResponseBodyAdvice로 응답 한 곳에서 관리
    3. 에러 처리 컴포넌트

---
+++ src/main/java/kezul/fighting/common/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package kezul.fighting.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
+}
---
+++ src/main/java/kezul/fighting/common/response/ResponseStatus.java
@@ -0,0 +1,5 @@
+package kezul.fighting.common.response;
+
+public enum ResponseStatus {
+    SUCCESS, ERROR
+}
