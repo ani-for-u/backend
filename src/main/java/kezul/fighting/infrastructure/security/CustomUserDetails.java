689079ad9411372464d92db34f9ac491c9212f82
sanghoon <tkdgns5817@gmail.com>
2025-01-29 05:39:38 +0900

    [refactor][#2] CustomUserDetails 권한 집합 반환 방식 변겅

--- src/main/java/kezul/fighting/infrastructure/security/CustomUserDetails.java
+++ src/main/java/kezul/fighting/infrastructure/security/CustomUserDetails.java
@@ -2,10 +2,12 @@ package kezul.fighting.infrastructure.security;
 
 import kezul.fighting.domain.user.model.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class CustomUserDetails implements UserDetails {
 
@@ -17,10 +19,9 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-        collection.add(() -> user.getRole().name());
-        return collection;
+        return Collections.singleton(
+                new SimpleGrantedAuthority(user.getRole().name())
+        );
     }
 
     @Override
