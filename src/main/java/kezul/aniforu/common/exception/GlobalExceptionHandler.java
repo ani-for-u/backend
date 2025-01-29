22f28215e92215048f47b6677e5f4f97991d969b
sanghoon <tkdgns5817@gmail.com>
2025-01-29 10:31:04 +0900

    [refactor][#2] 변수명 수정

--- src/main/java/kezul/aniforu/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kezul/aniforu/common/exception/GlobalExceptionHandler.java
@@ -2,11 +2,8 @@ package kezul.aniforu.common.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kezul.aniforu.domain.user.exception.AuthErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,15 +16,10 @@ public class GlobalExceptionHandler {
     private final MessageSource messageSource;
 
     @ExceptionHandler(AniforuException.class)
-    public void handleFighterException(AniforuException e) {
+    public void handleAniforuException(AniforuException e) {
         setResponse(e.getErrorCode());
     }
 
-    @ExceptionHandler(AuthenticationException.class)
-    public void handleAuthenticationException() {
-        System.out.println("========================");
-        setResponse(AuthErrorCode.LOGINID_OR_PASSWORD_MISMATCH);
-    }
 
     private void setResponse(ErrorCode errorCode) {
         response.setStatus(errorCode.getStatus().value());
